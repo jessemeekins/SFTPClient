95ae1c05ae7b89c448708efe189177ff30f7df3a
jessemeekins <jessemeekins@me.com>
2023-10-12 09:28:14 -0500

    Updated clas Methods for greater effecieny

--- SFTP_Client.py
+++ SFTP_Client.py
@@ -20,13 +20,18 @@ stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 logger.addHandler(logging.FileHandler('SFTP_logs.log'))
 
-now=dt.now()
+now = dt.now()
 
 
 class SFTPClient(ABC):
     """SFTP Client Methods"""
-    def __init__(self, kwargs) -> None:
-        ...
+
+    def __init__(self, ftp_client, ssh_client, **config) -> None:
+        self.ftp_client = ftp_client
+        self.ssh_client = ssh_client
+        self.filename = config.get('filename', '')
+        self.extension = config.get('extension', '')
+        self.destination = config.get('destination', '')
 
     @classmethod
     def connect(cls, **kwargs):
@@ -34,25 +39,19 @@ class SFTPClient(ABC):
             ssh_client = paramiko.SSHClient()
             logging.info(f"[{now}] SSHClient initialized")
             ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh_client.connect(hostname=kwargs["location"], port=kwargs["port"], username=kwargs["username"], password=kwargs["password"], banner_timeout=200)
+            ssh_client.connect(hostname=kwargs.get("location"), port=kwargs.get("port"), username=kwargs.get("username"), password=kwargs.get("password"), banner_timeout=200)
             ftp_client = ssh_client.open_sftp()
-            return cls(ftp_client, ssh_client, kwargs)
+            return cls(ftp_client, ssh_client, **kwargs)
         except Exception as e:
             logging.error(f"[*] Connection failed at [{now}]")
             logging.error(f"[*] {e}")
 
     def file_transfer(self):
         """File transfer"""
-     
-class SFTPGet(SFTPClient):
-    def __init__(self, ftp_client, ssh_client, kwargs):
-        super().__init__(kwargs)
-        self.ftp_client = ftp_client
-        self.ssh_client = ssh_client
-        self.filename = kwargs['filename']
-        self.extension = kwargs["extension"]
-        self.destination = kwargs['destination']
+        pass
 
+
+class SFTPGet(SFTPClient):
     def file_transfer(self) -> None:
         """Get Method"""
         try:
@@ -65,17 +64,9 @@ class SFTPGet(SFTPClient):
             logging.error(f"[{now}] File not downloaded...")
             self.ftp_client.close()
         self.ssh_client.close()
-        
+
 
 class SFTPPut(SFTPClient):
-    def __init__(self, ftp_client, ssh_client, kwargs):
-        super().__init__(kwargs)
-        self.ftp_client = ftp_client
-        self.ssh_client = ssh_client
-        self.filename = kwargs['filename']
-        self.extension = kwargs["extension"]
-        self.destination = kwargs['destination']
-
     def file_transfer(self) -> None:
         """Post Method"""
         try:
@@ -90,17 +81,16 @@ class SFTPPut(SFTPClient):
             self.ftp_client.close()
             self.ssh_client.close()
 
-        
-
 
 def sftp_main(config_name):
     config = FileConfig(config_name).get_file_data()
-    if config["transfer_type"] == "get":
+    transfer_type = config.get("transfer_type", "")
+    if transfer_type == "get":
         sftp_client = SFTPGet.connect(**config)
-    elif config["transfer_type"] == "put":
+    elif transfer_type == "put":
         sftp_client = SFTPPut.connect(**config)
     else:
-        logging.error(f"[{now}] Unknown transfer type: {config['transfer_type']}")
+        logging.error(f"[{now}] Unknown transfer type: {transfer_type}")
         return
 
     try:
@@ -109,8 +99,3 @@ def sftp_main(config_name):
         logging.error(f"[*] {e}")
     finally:
         del sftp_client
-
-
-
-
-
